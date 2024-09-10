a9b46a52daa2159e221fb9f41ea00ac75a249fb0
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2024-09-10 14:17:12 +0700

    fix path to vercel

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.listen(port, () => {
 })
 
 async function calculate(areaCode, subAreaCode) {
-    const data = readFileSync('./area.json');
+    const data = readFileSync(process.cwd() + '/area.json', 'utf8');
     const parsed = (JSON.parse(data))
 
     let areaName
