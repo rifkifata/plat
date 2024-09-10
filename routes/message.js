15f2ffbe42b9551315d555e665f63e1af00399f6
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2024-09-10 11:41:58 +0700

    vercel

---
+++ routes/message.js
@@ -0,0 +1,8 @@
+module.exports = {
+    emptyAreaCode: "areaCode cannot be empty",
+    notFoundAreaCode: "areaCode not found in our database",
+    notFoundSubAreaCode: "subAreaCode not found in our database",
+    numberValidation: "Only number allowed",
+    charValidation: "Number and special character is not allowed",
+    spaceValidation: "Space is not allowed"
+}
